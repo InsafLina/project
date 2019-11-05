a7711e1b46d3acd14c9e6af18431064aec68bd59
insafmechergui <insaf_mechergui@outlook.com>
2019-11-05 11:33:34 +0100

    css add item to the cart display cart

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 var arrayItems = [] ;
 
-function Items(category, type, color, size, picture) {
+function Items(category, type, color, size, price, picture) {
 	var item = {};
 
 	item.category = category;
 	item.type = type;
 	item.color = color;
 	item.size = size;
+	item.price = price;
 	item.picture = picture;
 
 	return item;
@@ -18,10 +19,10 @@ var image2 = $('<img>').attr( "src", "images/2.jpg");
 var image3 = $('<img>').attr( "src", "images/3.jpg");
 var image4 = $('<img>').attr( "src", "images/4.jpg");
 
-var shoes1 = Items("men", "shoes", "brown", 40, image1);
-var shoes2 = Items("men", "shoes", "black", 42, image2);
-var shoes3 = Items("woman", "shoes", "black", 38, image3);
-var shoes4 = Items("woman", "shoes", "black", 36, image4);
+var shoes1 = Items("men", "shoes", "brown", 40, 60, image1);
+var shoes2 = Items("men", "shoes", "black", 42, 70, image2);
+var shoes3 = Items("woman", "shoes", "black", 38, 80, image3);
+var shoes4 = Items("woman", "shoes", "black", 36, 95, image4);
 
 
 arrayItems.push(shoes1, shoes2, shoes3, shoes4);
